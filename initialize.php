d646500b7091621554a25a1b411a2a136b001b52
Mauricio Sevilla Britto <msevillab@gmail.com>
2022-01-22 23:38:08 -0500

    se actualiza url

--- initialize.php
+++ initialize.php
@@ -1,6 +1,6 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Mauricio','lastname'=>'Sevilla','username'=>'configuroweb','password'=>'4b67deeb9aba04a5b54632ad19934f26','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/celular/');
+if(!defined('base_url')) define('base_url','http://localhost/taller_soporte/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
